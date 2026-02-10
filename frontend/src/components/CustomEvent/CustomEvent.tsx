0fddca75633b06e044852b84f99fcc2c17a4e585
Thomas Candille <thomascandille0508@gmail.com>
2026-02-10 10:54:22 +0100

    feat: add CustomEvent and ValidateEvents components with styling, integrate into Calendar and Header, random fixs

---
+++ frontend/src/components/CustomEvent/CustomEvent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CustomEvent.css";
+
+const CustomEvent = ({}: {}) => {
+    return (
+        <div className="custom-event">
+            <h3>Custom Event</h3>
+            <button className="custom_event_button">Create Custom Event</button>
+        </div>
+    );
+}
+
+export default CustomEvent;
